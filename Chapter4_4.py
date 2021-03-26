d8d8f859f5bdfe32bb0ec6546d884862401ff469
Maciej <maciejzadworny@gmail.com>
2021-03-26 21:59:00 +0000

    Fixed errors

--- Chapter4_4.py
+++ Chapter4_4.py
@@ -1,15 +1,16 @@
-y = int
 
 def divider(x):
   x = x / 2
   x = int(x)
-  print(x)
+  return x 
 
 def multiplier(x):
   x = x * 4
   x = int(x)
-  print(x)
+  return x
 
-y = divider(6)
+g = input("GIVE ME A NUMBER! ")
+g = int(g)
+y = divider(g)
 p = multiplier(y)
 print(p)
